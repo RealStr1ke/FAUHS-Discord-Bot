7903f978b047c23ac3019c2a8c84db224ff1bdcb
Thandi Menelas <TMenelas@outlook.com>
2021-10-08 17:19:20 -0400

    Added two commands, ?ping and ?help

--- src/index.js
+++ src/index.js
@@ -6,9 +6,21 @@ const { token } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+  });
+
+// ?ping command
+client.on('message', msg => {
+	if (msg.content === '?ping') {
+	  msg.reply('The bot is online!');
+	}
+
+// ?help command
+client.on('message', msg => {
+	if (msg.content === '?help') {
+	  msg.reply('?ping - See if the bot is online');
+	}
 
 // Login to Discord with your client's token
 client.login(token);
