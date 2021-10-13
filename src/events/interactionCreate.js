e3fa02b26022472a0f32c12492ed07e46e72392e
Thandi Menelas <TMenelas@outlook.com>
2021-10-13 14:05:26 -0400

    Added the BetterDiscordPanel and fixed some events

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -2,5 +2,5 @@ module.exports = (client, message) => {
 	name: 'interactionCreate',
 	execute(interaction) {
 		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
-	},
+	}
 };
