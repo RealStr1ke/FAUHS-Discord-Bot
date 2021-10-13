84fac4a33a76008bf9950d190c15da99772ff987
RealStr1ke <>
2021-10-13 14:04:48 +0000

    Added new events and commands, disabled deploy-commands.js in favor of a text command handler

--- index.js
+++ index.js
@@ -3,39 +3,26 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./config.json");
+client.config = config;
+client.commands = new Discord.Collection();
 
-
-client.commands = new Collection();
-const slashCommandFiles = fs.readdirSync('./src/commands/slash').filter(file => file.endsWith('.js'));
-const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const event = require(`./src/events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
-for (const file of slashCommandFiles) {
-	const command = require(`./src/commands/slash/${file}`);
-	client.commands.set(command.data.name, command);
-}
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+fs.readdir("./src/events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./src/events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+fs.readdir("./src/commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./src/commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
 });
 
 client.login(config.token);
--- src/events/ready.js
+++ src/events/ready.js
@@ -3,5 +3,6 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+        console.log(`Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
 	},
 };
