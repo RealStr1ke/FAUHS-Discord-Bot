d0ef856b4eb57fb9fd49e06db88736df4018d17b
Thandi Menelas <TMenelas@outlook.com>
2021-10-13 13:26:53 -0400

    "Added new packages and edited index.js"

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+/* const { Client, Intents } = require('discord.js');
 const Discord = require("discord.js");
 const fs = require("fs");
 
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
 const config = require("./config.json");
 client.config = config;
 client.commands = new Discord.Collection();
@@ -14,6 +15,39 @@ fs.readdir("./src/events/", (err, files) => {
     client.on(eventName, event.bind(null, client));
   });
 });
+fs.readdir("./src/commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./src/commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+client.login(config.token); */
+
+const { Client, Intents } = require('discord.js');
+const Discord = require("discord.js");
+const fs = require("fs");
+
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
+const config = require("./config.json");
+// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+client.config = config;
+
+fs.readdir("./src/events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./src/events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Discord.Collection();
+
 fs.readdir("./src/commands/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
