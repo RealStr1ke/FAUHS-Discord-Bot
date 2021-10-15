2553e42597b499603d64e995da2ee21dd4120a9f
tmenelas2021 <tmenelas2021@fau.edu>
2021-10-15 10:14:02 -0400

    Updated modules, events, commands (regular and slash) with guidebot templates

--- index.js
+++ index.js
@@ -1,74 +1,91 @@
-/* const { Client, Intents } = require('discord.js');
-const Discord = require("discord.js");
-const fs = require("fs");
+// This will check if the node version you are running is the required
+// Node version, if it isn't it will throw the following error to inform
+// you.
+if (Number(process.version.slice(1).split(".")[0]) < 16) throw new Error("Node 16.x or higher is required. Update Node on your system.");
+require("dotenv").config();
 
-const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
-const config = require("./config.json");
-client.config = config;
-client.commands = new Discord.Collection();
+// Load up the discord.js library
+const { Client, Collection } = require("discord.js");
+// We also load the rest of the things we need in this file:
+const { readdirSync } = require("fs");
+const { intents, partials, permLevels } = require("./config.js");
+const logger = require("./modules/Logger.js");
+// This is your client. Some people call it `bot`, some people call it `self`,
+// some might call it `cootchie`. Either way, when you see `client.something`,
+// or `bot.something`, this is what we're referring to. Your client.
+const client = new Client({ intents, partials });
 
-fs.readdir("./src/events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./src/events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
-fs.readdir("./src/commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./src/commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
+// Aliases, commands and slash commands are put in collections where they can be
+// read from, catalogued, listed, etc.
+const commands = new Collection();
+const aliases = new Collection();
+const slashcmds = new Collection();
 
-client.login(config.token); */
+// Generate a cache of client permissions for pretty perm names in commands.
+const levelCache = {};
+for (let i = 0; i < permLevels.length; i++) {
+  const thisLevel = permLevels[i];
+  levelCache[thisLevel.name] = thisLevel.level;
+}
 
-const { Client, Intents } = require('discord.js');
-const Discord = require("discord.js");
-const fs = require("fs");
+// To reduce client pollution we'll create a single container property
+// that we can attach everything we need to.
+client.container = {
+  commands,
+  aliases,
+  slashcmds,
+  levelCache
+};
 
-const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
-const config = require("./config.json");
-// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
-client.config = config;
+// We're doing real fancy node 8 async/await stuff here, and to do that
+// we need to wrap stuff in an anonymous function. It's annoying but it works.
 
-fs.readdir("./src/events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    const event = require(`./src/events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
+const init = async () => {
 
-client.commands = new Discord.Collection();
-
-fs.readdir("./src/commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./src/commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
-
-client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-  if (message.content.startsWith(config.prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(config.prefix + "foo")) {
-    message.channel.send("bar!");
+  // Here we load **commands** into memory, as a collection, so they're accessible
+  // here and everywhere else.
+  const commands = readdirSync("./src/commands/").filter(file => file.endsWith(".js"));
+  for (const file of commands) {
+    const props = require(`./src/commands/${file}`);
+    logger.log(`Loading Command: ${props.help.name}. 👌`, "log");
+    client.container.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.container.aliases.set(alias, props.help.name);
+    });
   }
-});
 
-client.login(config.token);
+  // Now we load any **slash** commands you may have in the ./slash directory.
+  const slashFiles = readdirSync("./src/slash").filter(file => file.endsWith(".js"));
+  for (const file of slashFiles) {
+    const command = require(`./src/slash/${file}`);
+    const commandName = file.split(".")[0];
+    logger.log(`Loading Slash command: ${commandName}. 👌`, "log");
+    
+    // Now set the name of the command with it's properties.
+    client.container.slashcmds.set(command.commandData.name, command);
+  }
+
+  // Then we load events, which will include our message and ready event.
+  const eventFiles = readdirSync("./src/events/").filter(file => file.endsWith(".js"));
+  for (const file of eventFiles) {
+    const eventName = file.split(".")[0];
+    logger.log(`Loading Event: ${eventName}. 👌`, "log");
+    const event = require(`./src/events/${file}`);
+    // Bind the client to any event, before the existing arguments
+    // provided by the discord.js event. 
+    // This line is awesome by the way. Just sayin'.
+    client.on(eventName, event.bind(null, client));
+  }  
+
+  // Threads are currently in BETA.
+  // This event will fire when a thread is created, if you want to expand
+  // the logic, throw this in it's own event file like the rest.
+  client.on("threadCreate", (thread) => thread.join());
+
+  // Here we login the client.
+  client.login();
+
+// End top-level async/await function.
+};
+
+init();
--- src/commands/embed.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, args) => {
-  if(!args || args.length < 1) return message.reply("Must provide a command name to reload.");
-
-message.channel.send({ embed: {
-    color: 3447003,
-    description: args
-  }
-});}
