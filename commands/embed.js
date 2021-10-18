fa57dc8227616be33c4027b8fa8ab8eb2b74b4fb
Thandi M <49248449+RealStr1ke@users.noreply.github.com>
2021-10-18 15:11:33 +0000

    Fixed packages and added embed command for testing

---
+++ commands/embed.js
@@ -0,0 +1,69 @@
+const config = require("../config.js");
+const { settings } = require("../modules/settings.js");
+exports.run = async (client, message, args, level) => {
+  const friendly = config.permLevels.find(l => l.level === level).name;
+const exampleEmbed = {
+	color: 0x0099ff,
+	title: 'Some title',
+	url: 'https://discord.js.org',
+	author: {
+		name: 'Some name',
+		icon_url: 'https://i.imgur.com/AfFp7pu.png',
+		url: 'https://discord.js.org',
+	},
+	description: 'Some description here',
+	thumbnail: {
+		url: 'https://i.imgur.com/AfFp7pu.png',
+	},
+	fields: [
+		{
+			name: 'Regular field title',
+			value: 'Some value here',
+		},
+		{
+			name: '\u200b',
+			value: '\u200b',
+			inline: false,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+	],
+	image: {
+		url: 'https://i.imgur.com/AfFp7pu.png',
+	},
+	timestamp: new Date(),
+	footer: {
+		text: 'Some footer text here',
+		icon_url: 'https://i.imgur.com/AfFp7pu.png',
+	},
+};
+const replying = settings.ensure(message.guild.id, config.defaultSettings).commandReply;
+  message.reply({ embeds: [exampleEmbed], allowedMentions: { repliedUser: (replying === "true") }});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["em"],
+  permLevel: "Bot Owner"
+};
+
+exports.help = {
+  name: "embed",
+  category: "Miscellaneous",
+  description: "Reply with test embed.",
+  usage: "embed"
+};
