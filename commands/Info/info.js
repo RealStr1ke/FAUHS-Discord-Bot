3bc6aae583c310e20e61ca346c913f94a90c4034
Thandi M <49248449+RealStr1ke@users.noreply.github.com>
2021-10-26 15:57:06 +0000

    Pushing all saved files

---
+++ commands/Info/info.js
@@ -0,0 +1,46 @@
+const config = require("../config.js");
+const { settings } = require("../../modules/settings.js");
+exports.run = async (client, message, args, level) => {
+  const friendly = config.permLevels.find(l => l.level === level).name;
+  const replying = settings.ensure(message.guild.id, config.defaultSettings).commandReply;
+  const infoEmbed = {
+	color: 0x0099ff,
+	title: 'Bot Info',
+	url: 'https://github.com/RealStr1ke/FAUHS-Discord-Bot/',
+	author: {
+		name: 'RealStr1ke',
+		icon_url: 'https://github.com/RealStr1ke',
+		url: 'https://cdn.discordapp.com/avatars/411641088944766982/5489f885a60acfa2c50cca1fddd458d5.png?size=1024',
+	},
+	description: `This bot was made with the puspose to cure Str1ke's boredom and help students.`,
+	thumbnail: {
+		url: 'https://pbs.twimg.com/profile_images/1146392144480858112/00X-bHE0_400x400.png',
+	},
+	fields: [
+		{
+			name: 'Repository:',
+			value: 'https://github.com/RealStr1ke/FAUHS-Discord-Bot',
+		}
+	],
+	timestamp: new Date(),
+	footer: {
+		text: 'Imagine reading the footer text',
+		icon_url: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg',
+	},
+};
+  message.reply({ embeds: [infoEmbed], allowedMentions: { repliedUser: (replying === "true") }});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["inf", "i"],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "info",
+  category: "Miscellaneous",
+  description: "Shows you the bot info and links.",
+  usage: "info`"
+};
--- commands/Miscellaneous/perms.js
+++ commands/Miscellaneous/perms.js
@@ -1,21 +1,21 @@
 const config = require("../config.js");
-const { settings } = require("../modules/settings.js");
+const { settings } = require("../../modules/settings.js");
 exports.run = async (client, message, args, level) => {
   const friendly = config.permLevels.find(l => l.level === level).name;
   const replying = settings.ensure(message.guild.id, config.defaultSettings).commandReply;
-  message.reply({ content: `**Incomplete Command**`, allowedMentions: { repliedUser: (replying === "true") }});
+  message.reply({ content: `Your permission level is: ${level} - ${friendly}`, allowedMentions: { repliedUser: (replying === "true") }});
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ["inf", "i"],
+  aliases: ["pl", "permlevel", "permsL"],
   permLevel: "User"
 };
 
 exports.help = {
-  name: "info",
+  name: "perms",
   category: "Miscellaneous",
-  description: "Shows you the bot info and links.",
-  usage: "info"
+  description: "Tells you your permission level for the current message location.",
+  usage: "perms"
 };
