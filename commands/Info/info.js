4ffc8e8ba24d97576e61703efa3e8441134ff97d
Thandi M <49248449+RealStr1ke@users.noreply.github.com>
2021-10-26 22:39:12 +0000

    Fixed code inconsistencies

--- commands/Info/info.js
+++ commands/Info/info.js
@@ -1,7 +1,6 @@
 const config = require("../../config.js");
 const { settings } = require("../../modules/settings.js");
 exports.run = async (client, message, args, level) => {
-  const friendly = config.permLevels.find(l => l.level === level).name;
   const replying = settings.ensure(message.guild.id, config.defaultSettings).commandReply;
   const infoEmbed = {
 	color: 0x0099ff,
